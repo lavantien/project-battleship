2adc4e150632cfd452a53cc94a383a6673f699c9
lavantien <lavantien96@gmail.com>
2018-11-28 23:21:51 +0700

    first commit

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { GameComponent } from './game/game.component';
+import { ForgotComponent } from './forgot/forgot.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'forgot',
+    component: ForgotComponent
+  },
+  {
+    path: 'game',
+    component: GameComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
